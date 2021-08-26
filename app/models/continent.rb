38e00d12e302b8155b7d0647c34d39d5501d89c4
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-08-26 10:25:49 -0400

    Added associations in models

--- app/models/continent.rb
+++ app/models/continent.rb
@@ -1,3 +1,4 @@
 class Continent < ActiveRecord::Base
-
+  # https://guides.rubyonrails.org/association_basics.html#the-has-many-association
+  has_many :countries
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,4 @@
 class Country < ActiveRecord::Base
-
+  # https://guides.rubyonrails.org/association_basics.html#the-belongs-to-association
+  belongs_to :continent
 end
