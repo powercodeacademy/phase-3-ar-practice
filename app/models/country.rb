ae354290306ec1e209f9abbd95376a638ae6e308
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-08-26 11:15:13 -0400

    Finish #winners_circle?

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,22 @@
 class Country < ActiveRecord::Base
   # https://guides.rubyonrails.org/association_basics.html#the-belongs-to-association
   belongs_to :continent
+
+  # If #winners_circle? is working:
+  #   - it will return `true` if a country has more than 50 `olympic_medals`
+  #   - it will return `false` if a country has less than 50 `olympic_medals`.
+  # SETUP IN CONSOLE:
+  #   - Start a console session
+  #     $ bundle exec rake console
+  #   - Verify that we have at least 2 countries: one with > 50 `olympic_medals`, one with < 50 `olympic_medals`)
+  #     Country.all
+  #   - Once we have two of them, set them to variables
+  #     france = Country.find_by(name: "France")
+  #     germany = Country.find_by(name: "Germany")
+  # RUN THE TESTS (VERIFY IT WORKED):
+  #   - expect `france.winners_circle?` to be true
+  #   - expect `germany.winners_circle?` to be false
+  def winners_circle?
+    olympic_medals > 50
+  end
 end
